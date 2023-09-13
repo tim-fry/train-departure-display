0a0cd702ab07ada9189d7d395e6c6d90b9fc66f7
Tim Fry <tim@frys.me.uk>
2023-09-13 20:19:42 +0100

    Revert "Allow emulation"
    This reverts commit 7742ef8656c2597c1c4c22aeb21a1fe578fb5abb.

--- src/main.py
+++ src/main.py
@@ -8,9 +8,9 @@ from trains import loadDeparturesForStation
 from config import loadConfig
 from open import isRun
 
-from helpers import get_device
-
+from luma.core.interface.serial import spi
 from luma.core.render import canvas
+from luma.oled.device import ssd1322
 from luma.core.virtual import viewport, snapshot, hotspot
 from luma.core.sprite_system import framerate_regulator
 
@@ -323,7 +323,11 @@ try:
     print('Starting Train Departure Display v' + version_file.read())
     config = loadConfig()
 
-    device = get_device()
+    serial = spi(port=0)
+    device = ssd1322(serial, mode="1", rotate=config['screenRotation'])
+    if config['dualScreen'] == True:
+        serial1 = spi(port=1,gpio_DC=5, gpio_RST=6)
+        device1 = ssd1322(serial1, mode="1", rotate=config['screenRotation'])
     font = makeFont("Dot Matrix Regular.ttf", 10)
     fontBold = makeFont("Dot Matrix Bold.ttf", 10)
     fontBoldTall = makeFont("Dot Matrix Bold Tall.ttf", 10)
